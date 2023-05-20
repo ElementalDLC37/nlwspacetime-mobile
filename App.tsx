31aad39b8646c3eb57811d663e17611195c73b2a
Rui Gomes <lucasantony815@gmail.com>
2023-05-20 11:52:08 -0300

    styless

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import { StatusBar } from 'expo-status-bar'
-import { ImageBackground, Text, View } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 
 import bgBlur from './src/assets/bg-blur.png'
 import Stripes from './src/assets/stripes.svg'
+import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
 
 import {
   useFonts,
@@ -29,11 +30,34 @@ export default function App() {
   return (
     <ImageBackground
       source={bgBlur}
-      className="flex-1 items-center justify-center bg-gray-900"
+      className="flex-1 items-center justify-center bg-gray-900 px-8 py-10"
       imageStyle={{ position: 'absolute', left: '-100%' }}
     >
       <StyledStripes className="absolute left-2 " />
-      <View className="flex-1 items-center justify-center gap-6"></View>
+      <View className="flex-1 items-center justify-center gap-6">
+        <NLWLogo />
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm font-bold uppercase text-black">
+            COMEÇAR A CADASTRAR
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
       <StatusBar style="light" translucent />
     </ImageBackground>
   )
