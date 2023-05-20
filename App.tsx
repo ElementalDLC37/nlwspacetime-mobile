8e713a3c50a14b2ead4a8d2a5e294f70fd05c6f9
Rui Gomes <lucasantony815@gmail.com>
2023-05-20 02:39:47 -0300

    app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { ImageBackground, Text, View } from 'react-native'
+
+import bgBlur from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar />
-    </View>
-  ); 
+    <ImageBackground
+      source={bgBlur}
+      className="flex-1 items-center justify-center bg-gray-900"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <Stripes />
+      <StatusBar style="light" translucent />
+    </ImageBackground>
+  )
 }
